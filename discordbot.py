86d2456f67a00b418f275183ac9a815fc613645e
Kirito0123-sao <69139831+Kirito0123-sao@users.noreply.github.com>
2020-08-03 17:15:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,9 +22,5 @@ async def ping(ctx):
 async def say(ctx, arg):
     await ctx.send(arg)
 
-    
-@bot.command()
-async def test(ctx, *, arg):
-    await ctx.send(arg)    
-       
+
 bot.run(token)
