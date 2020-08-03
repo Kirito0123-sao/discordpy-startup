eb567e2622cc50c092530b68bb748fa11e2785ed
Kirito0123-sao <69139831+Kirito0123-sao@users.noreply.github.com>
2020-08-03 21:12:40 +0900

    Update debug.py

--- debug.py
+++ debug.py
@@ -11,6 +11,29 @@ from discord import NotFound, Embed,  Forbidden
 from .detabase import database
 from all_data.all_data import admin_list, prefix 
  
+ 
+ def cleanup_code(content):
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+    return content.strip('` \n')
+
+def get_syntax_error(e):
+    if e.text is None:
+        return f'```py\n{e.__class__.__name__}: {e}\n```'
+    return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+
+def mention_to_user_id(mention):
+    user_id = mention.strip("<@").strip(">")
+    if user_id.find("!") != -1:
+        user_id = user_id.strip("!")
+    return int(user_id)
+
+class debug(commands.Cog): #ここのdebugはhelpの時に[{prefix}help コマンド名]としたときにこのファイルのコマンドが指定されたときにクラス名を取得するので変える場合はhelpの中も変えてください。
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_result = None
+ 
+ 
  @commands.command(name='eval', pass_context=True, description="※運営専用コマンド") # コマンド名:『eval』 省略コマンド:『なし』
     @commands.bot_has_permissions(read_messages=True, send_messages=True, embed_links=True, add_reactions=True, manage_messages=True, read_message_history=True) #これ絶対消しちゃダメ
     async def evals(self, ctx): #既に存在する関数名だったらERROR出るのでもし今後コマンドを追加するならコマンド名と同じ関数名にして下さい。(ここは例外)
