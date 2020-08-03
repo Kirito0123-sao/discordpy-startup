478e2a8e988b12e4ebb2e5f4da38e35202952dfe
Kirito0123-sao <69139831+Kirito0123-sao@users.noreply.github.com>
2020-08-03 21:58:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -72,41 +72,6 @@ async def eval_(ctx, *, cmd):
             await ctx.send([e])
             if ctx.message is not None:await ctx.message.add_reaction("❓")
 
- @commands.command()
-    async def maru(self, ctx, *, member: discord.Member = None):
-        """アバターを丸にする"""
 
-        # ユーザーが指定されていなかった場合、メッセージを送信したユーザーを使用します。
-        member = member or ctx.author
-
-        # 処理をしながら「入力中」を表示する
-        async with ctx.typing():
-            if isinstance(member, discord.Member):
-                # サーバーにいるならユーザーの表示色を取得する
-                member_colour = member.colour.to_rgb()
-            else:
-                # DMなどにいるなら(0, 0, 0)を使用する
-                member_colour = (0, 0, 0)
-
-            # アバターデータを bytes として取得。
-            avatar_bytes = await self.get_avatar(member)
-
-            # partialオブジェクトを作る
-            # fnが呼び出されると、avatar_bytesとmember_colorを引数として渡してself.processingを実行するのと同様の動作をします。
-            fn = partial(self.processing, avatar_bytes, member_colour)
-
-            # executorを使ってfnを別スレッドで実行します。
-            # こうやって非同期的に関数が返すまで待つことができます
-            # final_bufferはself.processingが返すバイトストリームになります。
-            final_buffer = await self.bot.loop.run_in_executor(None, fn)
-
-            # ファイル名「maru.png」の指定とfinal_bufferの内部でファイルを準備して
-            file = discord.File(filename="maru.png", fp=final_buffer)
-
-            # 最後にファイルをアップします。
-            await ctx.send(file=file)
-
-
-def setup(bot: commands.Bot):
 
 bot.run(token)
